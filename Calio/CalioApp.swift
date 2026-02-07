78d2507f33548572100dbb029396f0df88b52a02
Kevin Kopczynski <kevin@kkaud.io>
2026-02-07 14:06:20 -0500

    Initial Commit

---
+++ Calio/CalioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalioApp.swift
+//  Calio
+//
+//  Created by Kevin Kopczynski on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CalioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
